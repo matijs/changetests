f293f2235b34e1ea635d4225d978721c0383ca42
matijs <matijs@gmail.com>
2024-05-03 08:39:14 +0200

    feat: make `id` mandatory on input
    Make sure developers associate a `label` with the `input` by making the
    `id` prop mandatory. This is a breaking change.

--- packages/input/src/Input.tsx
+++ packages/input/src/Input.tsx
@@ -4,10 +4,17 @@ import { clsx } from "clsx";
 interface Props
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "className" | "style"> {
   appearance?: "error";
+  id: "string";
 }
 
-export const Input = ({ appearance, type = "text", ...restProps }: Props) => (
+export const Input = ({
+  appearance,
+  id,
+  type = "text",
+  ...restProps
+}: Props) => (
   <input
+    id={id}
     className={clsx("input", { ["input--error"]: appearance === "error" })}
     type={type}
     {...restProps}
