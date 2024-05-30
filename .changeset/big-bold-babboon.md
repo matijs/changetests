2006899249471749106e0a7de1b9634db9c173dd
matijs <matijs@gmail.com>
2024-05-30 20:41:11 +0200

    feat: turn FormField into dumb component

---
+++ .changeset/big-bold-babboon.md
@@ -0,0 +1,5 @@
+---
+'@matijs/form-field': major
+---
+
+Turn the component into a dumb component
--- packages/form-field/src/FormField.tsx
+++ packages/form-field/src/FormField.tsx
@@ -1,18 +1,7 @@
 import type { PropsWithChildren } from 'react';
-import { createContext } from 'react';
-
-interface FormFieldContext {
-  hasError: boolean;
-}
-
-const initialValue: FormFieldContext = {
-  hasError: false,
-};
-
-const Context = createContext<FormFieldContext>(initialValue);
 
 type Props = PropsWithChildren<{}>;
 
 export function FormField({ children }: Props) {
-  return <Context.Provider value={initialValue}>{children}</Context.Provider>;
+  return <div className="form-field">{children}</div>;
 }
