caa048f3aa1dd784df7c6493031d42a057c25408
matijs <matijs@gmail.com>
2024-05-30 15:25:40 +0200

    feat: add form-field

---
+++ .changeset/lucky-grapes-cough.md
@@ -0,0 +1,5 @@
+---
+"@matijs/form-field": minor
+---
+
+Initial release for form-field
---
+++ packages/form-field/src/FormField.tsx
@@ -0,0 +1,18 @@
+import type { PropsWithChildren } from "react";
+import { createContext } from "react";
+
+interface FormFieldContext {
+  hasError: boolean;
+}
+
+const initialValue: FormFieldContext = {
+  hasError: false,
+};
+
+const Context = createContext<FormFieldContext>(initialValue);
+
+type Props = PropsWithChildren<{}>;
+
+export function FormField({ children }: Props) {
+  return <Context.Provider value={initialValue}>{children}</Context.Provider>;
+}
