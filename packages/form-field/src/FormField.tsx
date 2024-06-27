1f334e4b0f51d3286ad23f68813dcbc6f976426c
matijs <matijs@gmail.com>
2024-06-27 17:08:28 +0200

    remove packages

--- packages/form-field/src/FormField.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from 'react';
-
-type Props = PropsWithChildren<{}>;
-
-export function FormField({ children }: Props) {
-  return <div className="form-field">{children}</div>;
-}
